f8e8fd5e1c6bf361465a4758d5bf08b4002bb42f
JiangJianghao <469672930@qq.com>
2017-12-08 15:20:15 +0800

    重载运算符和重载函数
    重载运算符和重载函数

---
+++ project/027_reloadOperatorAndFunction/027_reloadOperatorAndFunction.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class printData
+{
+public:
+	void print(int i){
+		cout << "int i:" << i << endl;
+	};
+
+	void print(string c){
+		cout << "string c:" << c << endl;
+	};
+
+	void print(double d){
+		cout << "double d:" << d << endl;
+	};
+
+private:
+
+};
+
+
+//运算符重载
+class Box
+{
+public:
+
+	double getVolume(void)
+	{
+		return length * breadth * height;
+	}
+	void setLength(double len)
+	{
+		length = len;
+	}
+
+	void setBreadth(double bre)
+	{
+		breadth = bre;
+	}
+
+	void setHeight(double hei)
+	{
+		height = hei;
+	}
+	// 重载 + 运算符，用于把两个 Box 对象相加
+	Box operator+(const Box& b)
+	{
+		Box box;
+		box.length = this->length + b.length;
+		box.breadth = this->breadth + b.breadth;
+		box.height = this->height + b.height;
+		return box;
+	}
+private:
+	double length;      // 长度
+	double breadth;     // 宽度
+	double height;      // 高度
+};
+
+int main(){
+	printData printdata;
+	printdata.print("sss");
+	printdata.print(123);
+	printdata.print(11.1);
+
+
+
+	//运算符重载
+	Box Box1;                // 声明 Box1，类型为 Box
+	Box Box2;                // 声明 Box2，类型为 Box
+	Box Box3;                // 声明 Box3，类型为 Box
+	double volume = 0.0;     // 把体积存储在该变量中
+
+	// Box1 详述
+	Box1.setLength(6.0);
+	Box1.setBreadth(7.0);
+	Box1.setHeight(5.0);
+
+	// Box2 详述
+	Box2.setLength(12.0);
+	Box2.setBreadth(13.0);
+	Box2.setHeight(10.0);
+
+	// Box1 的体积
+	volume = Box1.getVolume();
+	cout << "Volume of Box1 : " << volume << endl;
+
+	// Box2 的体积
+	volume = Box2.getVolume();
+	cout << "Volume of Box2 : " << volume << endl;
+
+	// 把两个对象相加，得到 Box3
+	Box3 = Box1 + Box2;
+
+	// Box3 的体积
+	volume = Box3.getVolume();
+	cout << "Volume of Box3 : " << volume << endl;
+
+	system("PAUSE");
+	return 0;
+}
